0566cc51dfa7ca317a32300b4a303c433f49b272
vitorklock <klockvitor@gmail.com>
2025-09-16 21:04:51 -0300

    antes do shadcn

---
+++ src/app/(dashboard)/coffes/page.tsx
@@ -0,0 +1,14 @@
+
+
+
+import React from 'react'
+
+function Page() {
+  return (
+    <div>
+        oi
+    </div>
+  )
+}
+
+export default Page
---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+
+
+
+
+import React, { ReactNode } from 'react'
+
+function Layout({ children }: {
+  children: ReactNode
+}) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Layout
